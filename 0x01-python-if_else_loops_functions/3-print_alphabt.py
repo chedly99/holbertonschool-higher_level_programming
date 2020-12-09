30d6329d178fe91d1507e712d542be0c8fe6c3ac
cehdly99 <mohamed.chedliiy@gmail.com>
2020-12-09 16:09:06 +0100

    print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) == 'q' or chr(i) == 'e':
-        continue
-    print("{:c}".format(i), end='')
-    
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
