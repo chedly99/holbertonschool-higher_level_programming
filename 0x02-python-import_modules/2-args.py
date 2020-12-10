c005e111f2675e9747c8915f7adca1dfa3fa97d2
cehdly99 <mohamed.chedliiy@gmail.com>
2020-12-10 20:10:26 +0100

    fixx

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    n = len(argv)
-    if n == 2:
+    arc = len(argv)
+    if arc > 2:
+        print("{:d} {:s}:".format(arc-1, "arguments"))
+        for i, s in enumerate(argv[1:]):
+            print("{:d}: {:s}".format(i+1, s))
+    if arc == 2:
         print("1 argument:")
         print("1: {}".format(argv[1]))
-    elif n == 1:
+    if arc == 1:
         print("0 arguments.")
-    else:
-        print("{} arguments:".format(n))  
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
