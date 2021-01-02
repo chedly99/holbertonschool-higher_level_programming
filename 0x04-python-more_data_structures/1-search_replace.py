18c2031f2a8147adcd715f69704c28c883f44937
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-02 17:51:17 +0100

    fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    result = 0
-    for x in set(my_list):
-        result += x
-    return (result)
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
