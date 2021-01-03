46bafb2608449dc428770fba06e1cdb03856d0c8
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-03 16:04:40 +0100

    fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    max_value = max(a_dictionary.values())
-    for key in a_dictionary.keys():
-        if a_dictionary[key] == max_value:
-            return key
+def multiply_by_2(a_dictionary):
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
