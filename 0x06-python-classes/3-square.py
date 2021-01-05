94cc2f65776d231b0d67391657a45a4b540f0886
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-05 21:21:30 +0100

    squaaree

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    documentation of the module square
+         1 - Square
+"""
+
+
+class Square:
+    """
+    documentation of the class Square
+        *methodes:
+            __init__
+            area
+        *attributes:
+            size: private
+    """
+    
+    def area(self):
+        """documentation of the method area: return the area of square"""
+        return self.__size**2
