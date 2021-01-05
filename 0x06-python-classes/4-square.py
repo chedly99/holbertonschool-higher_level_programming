091d3c5d60865779fa18de0f0de48605ad33ac06
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-05 22:22:51 +0100

    fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,3 +15,31 @@ class Square:
             None
         """
         self.size = size
+
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+
+            self.__size = value
