527f8ece70fbd13c5390a55f588ec48613e911b2
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-05 21:42:20 +0100

    fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,7 +14,14 @@ class Square:
         *attributes:
             size: private
     """
-    
+    def __init__(self, size=0):
+        """documentation of the method __init__: constructor"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
     def area(self):
         """documentation of the method area: return the area of square"""
         return self.__size**2
