72be9acba26c254f58e07abc5e0c1c34926547cf
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-05 20:29:51 +0100

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+define a class Square
+"""
+
+
+class Square:
+    """The summary line for a class docstring should fit on one line
+    """
+
+    def __init__(self, size):
+
+        """square class"""
+
+        self.__size = size
