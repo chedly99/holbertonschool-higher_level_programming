0567f32da7eb4852272a8b2fbcc379d8100a3470
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-11 22:35:17 +0100

    recto1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''class that defines new Rectangle '''
+
+
+class Rectangle:
+    '''new Class'''
+
+    def __init__(self, width=0, height=0):
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        ''' a property to retrieve the value of width '''
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        ''' property setter to set the value of width '''
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        ''' propoerty setter to set the value of hight '''
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
