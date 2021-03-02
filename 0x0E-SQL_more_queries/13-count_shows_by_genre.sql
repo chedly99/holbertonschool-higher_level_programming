8f6e3ab2c64b4c604a6fb7147157b90f4d7bc3c3
root <mohamed.chedliiy@gmail.com>
2021-03-02 17:02:02 +0000

    count shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- list all genres 
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
