0edc806deabb569e338840c5ebbd899f3af325ee
cehdly99 <mohamed.chedliiy@gmail.com>
2021-05-12 23:56:57 +0100

    had to

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(this).ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list :last-child').remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
