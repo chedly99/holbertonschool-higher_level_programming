6305c97994c6a2f23a33c765997c138d79c6ba14
chedly99 <mohamed.chedliiy@gmail.com>
2021-06-25 16:04:19 +0100

    update

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  fs.writeFile(file, body, function (err) { if (err) console.log(err); });
+});
