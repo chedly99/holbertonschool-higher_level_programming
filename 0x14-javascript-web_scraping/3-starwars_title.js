b75d13062cdc8ff43294c0482b235379d4a075f1
chedly99 <mohamed.chedliiy@gmail.com>
2021-06-25 16:02:10 +0100

    update

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+request(url, function (err, response, body) {
+  if (err) { console.error('error:', err); }
+  console.log(JSON.parse(body).title);
+});
